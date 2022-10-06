10eac8deeb9587791c2d75997f84dc1a364c9241
Beatriz Gonçalves <beatriz.bgoncalves@mercadolivre.com>
2022-10-06 00:30:22 -0300

    feat: add icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,20 @@
+import { Received, Reversal, Scheduled } from '../../assets/icons';
+
 const Header = () => {
-  return <h1>Extrato</h1>;
+  return (
+    <h1>
+      <i>
+        <Received />
+      </i>
+      <i>
+        <Reversal />
+      </i>
+      <i>
+        <Scheduled />
+      </i>
+      Extrato
+    </h1>
+  );
 };
 
 export default Header;
